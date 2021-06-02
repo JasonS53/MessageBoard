c0a7125c147e5d71a04e75a9d16fee598aafa632
Jason <j_schubb53@yahoo.com>
2021-06-02 16:30:06 -0400

    first commit

---
+++ MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using MessageBoard.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [HttpDelete]
+        public IActionResult Delete(int id, string message)
+        {
+            Message message1 = new Message();
+            using (MessageBoardDbContext context = new MessageBoardDbContext())
+            {
+                message1 = context.Message.ToList().Find(m => m.Id == id);
+                message1.Message1 = message;
+                context.Message.Remove(message1);
+                context.SaveChanges();
+            };
+            return RedirectToAction("MessageBoard");
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Edited(int id, string message)
+        {
+            Message message1 = new Message();
+            using (MessageBoardDbContext context = new MessageBoardDbContext())
+            {
+                message1 = context.Message.ToList().Find(m => m.Id == id);
+                message1.Message1 = message;
+                message1.Updated = true;
+                context.SaveChanges();
+            };
+            return RedirectToAction("MessageBoard");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            using (MessageBoardDbContext context = new MessageBoardDbContext())
+            {
+                Message message = new Message();
+                message = context.Message.ToList().Find(m => m.Id == id);
+                return View(message);
+            };
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult MessageBoard(string message)
+        {
+            Message message1 = new Message();
+            using (MessageBoardDbContext context = new MessageBoardDbContext())
+            {
+                message1.UserId = User.Identity.Name; 
+                message1.PostedTime = DateTime.Now;
+                message1.Message1 = message;
+                message1.Updated = false;
+                context.Message.Add(message1);
+                context.SaveChanges();
+            };
+            return Redirect("MessageBoard");
+        }
+
+        [Authorize]
+        public IActionResult MessageBoard()
+        {
+            List<Message> messageList = new List<Message>();
+            using (MessageBoardDbContext context = new MessageBoardDbContext())
+            {
+                messageList = context.Message.ToList();
+            }
+            return View(messageList);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MessageBoard/Models/MessageBoardDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace MessageBoard
+{
+    public partial class MessageBoardDbContext : DbContext
+    {
+        public MessageBoardDbContext()
+        {
+        }
+
+        public MessageBoardDbContext(DbContextOptions<MessageBoardDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Message> Message { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=MessageBoardDb; Integrated Security=SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message>(entity =>
+            {
+                entity.ToTable("message");
+
+                entity.Property(e => e.Message1)
+                    .HasColumnName("Message")
+                    .HasMaxLength(360);
+
+                entity.Property(e => e.PostedTime).HasColumnType("datetime");
+
+                entity.Property(e => e.UserId).HasMaxLength(255);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
